de91d606d2fcf7c5f43055f90e70afbfc4b8e069
Guido Schippers <guido_schippers@hotmail.com>
2022-11-14 17:44:24 +0100

    Boilerplate

---
+++ RemoteControl.Worker/Program.cs
@@ -0,0 +1,10 @@
+using RemoteControl.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
